1906ddee602e0359d34c71b99785b7eac8bf7995
Anuj_Web_Developer <ay5245193@gmail.com>
2025-01-27 12:38:37 +0530

    responsive

--- calc.js
+++ calc.js
@@ -1,31 +1,45 @@
- let input = document.querySelector("#input");
- let btn = document.querySelectorAll(".btn");
- 
- let buttonArray = Array.from(btn);
- let string = '';
+let input = document.querySelector("#input");
+let btn = document.querySelectorAll(".btn");
 
- buttonArray.forEach(btn=>{
-    btn.addEventListener("click",(e)=>{
+let buttonArray = Array.from(btn);
+let string = " ";
 
-        if(e.target.innerHTML == 'AC'){
-            string = string.substring(0,string.length-1);
-            input.innerHTML = string;
-        }
-        else if(e.target.innerHTML == 'X'){
-            string = '';
-            input.innerHTML = string;
-        }
-        else if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.innerHTML = string;
-        }
-else if (e.target.innerHTML == 'AC || X'){
-    string = e.target.innerHTML;
-}
-        else{
-            string+= e.target.innerHTML;
-        input.innerHTML = string;
-        }
-    });
- });
+buttonArray.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "AC") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else if (e.target.innerHTML == "X") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC || X") {
+      string = e.target.innerHTML;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
 
+// let t='';
+// let test = document.querySelector("#test");
+// test.addEventListener("keydown", (event) => {
+//     t+=(event.key);
+//     console.log(t);
+//   console.log(`key ${event.key} is pressed`);
+// });
+
+window.addEventListener("keydown",(event)=>{
+    if(event.key === "Enter"){
+        let result = eval(string);
+        input.value = result;
+       
+    }
+    else{
+        string+=event.key;
+        input.value = string;
+    }
+})
